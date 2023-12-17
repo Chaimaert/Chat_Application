d9ae80bf7ee08fb820a1bc6ed834b63e01edfd43
chaimaert <chaima.Rouita@emsi-edu.ma>
2023-12-17 14:50:21 +0100

    Update

--- Chat_App/src/com/raven/main/Main.java
+++ Chat_App/src/com/raven/main/Main.java
@@ -6,6 +6,8 @@ package com.raven.main;
 import com.raven.swing.ComponentResizer;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+
 
 /**
  *
@@ -22,12 +24,13 @@ public class Main extends javax.swing.JFrame {
     }
     
     private void init() {
+        setIconImage(new ImageIcon(getClass().getResource("/com/raven/icon/icon.png")).getImage());
         ComponentResizer com = new ComponentResizer();
         com.registerComponent(this); 
         com.setMinimumSize(new Dimension(800, 500));
         com.setMaximumSize(Toolkit.getDefaultToolkit().getScreenSize());  
         com.setSnapSize(new Dimension(10, 10));
-        setIconImage(new ImageIcon(getClass().getResource("/com/raven/icon.png")).getImage());
+        
     }
 
     /**
